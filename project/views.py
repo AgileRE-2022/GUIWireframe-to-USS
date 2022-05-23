55908694c8c7a3726f978aa3b5cb6e099c81adb6
daffahalif <daffa.halif.muhammad-2019@fst.unair.ac.id>
2022-05-23 08:15:58 +0700

    setup urls route dan views

--- project/views.py
+++ project/views.py
@@ -1,4 +1,3 @@
-
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render, redirect
@@ -36,3 +35,25 @@ def create(request):
     else:
         template = loader.get_template('project/create.html')
         return render(request, "project/create.html")
+
+def details(request, id):
+    return HttpResponse('page details dengan id = '+ str(id))
+
+def rulesAdd(request, id):
+    return HttpResponse('ini rules add dengan id = '+ str(id))
+
+def rulesEdit(request, id, rid):
+    return HttpResponse('ini page rules edit dari id ='+str(id)+' dengan rules id = '+str(rid))
+
+def activityAdd(request, id):
+    return HttpResponse('ini activity add dengan id = '+ str(id))
+
+def activityEdit(request, id, aid):
+    return HttpResponse('ini page activity edit dari id ='+str(id)+' dengan activity id = '+str(aid))
+
+def context(request, id):
+    return HttpResponse('page context dengan id = '+ str(id))
+
+def export(request, id):
+    return HttpResponse('page export dengan id = '+ str(id))
+
