801d556d35198a93762e3a8e312eb083c3f1b61d
yagrariksa <daffa.yagrariksa@gmail.com>
2022-05-23 23:20:47 +0700

    init html

--- project/views.py
+++ project/views.py
@@ -36,21 +36,25 @@ def create(request):
         template = loader.get_template('project/create.html')
         return render(request, "project/create.html")
 
+# dapa
 def details(request, id):
     return HttpResponse('page details dengan id = '+ str(id))
 
+# aril
 def rulesAdd(request, id):
     return HttpResponse('ini rules add dengan id = '+ str(id))
 
 def rulesEdit(request, id, rid):
     return HttpResponse('ini page rules edit dari id ='+str(id)+' dengan rules id = '+str(rid))
 
+# rapid
 def activityAdd(request, id):
     return HttpResponse('ini activity add dengan id = '+ str(id))
 
 def activityEdit(request, id, aid):
     return HttpResponse('ini page activity edit dari id ='+str(id)+' dengan activity id = '+str(aid))
 
+# ga dulu 
 def context(request, id):
     return HttpResponse('page context dengan id = '+ str(id))
 
