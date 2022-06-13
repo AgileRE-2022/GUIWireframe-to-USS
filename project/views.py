4b64230b9f6ef988dffeb4058e39d5c1a20a91ec
daffahalif <daffa.halif.muhammad-2019@fst.unair.ac.id>
2022-06-13 09:18:12 +0700

    GET Scenario, Given, When, Then dan di export dan download ke file=USS.feature

--- project/views.py
+++ project/views.py
@@ -434,9 +434,67 @@ def download(request, id):
     # Define Django project base directory
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     # Define text file name
-    filename = 'uss.txt'
+    filenametemp = 'usstemp.txt'
+    filename = 'uss.feature'
     # Define the full file path
-    filepath = BASE_DIR + '/project/functions/' + filename
+    filepathtemp = BASE_DIR + '/project/functions/' + filenametemp #template
+    filepath = BASE_DIR + '/project/functions/' + filename #ini yang di download
+
+    # disini ambil variabel, trs write file
+    wireframe = Wireframe.objects.get(id=id)
+    scenario =  Scenario.objects.filter(wireframe_id=id)
+
+    file1 = open(filepathtemp,"r")
+    text = file1.read()
+    a = "\tIn order to " + wireframe.project_us_purpose
+    b = "\tAs a " + wireframe.project_us_user
+    c = "\tI want to " + wireframe.project_us_todo
+    
+    text = text.replace('<a>',a)
+    text = text.replace('<b>',b)
+    text = text.replace('<c>',c)
+    p = ""
+    for scene in scenario:
+        s = "\tScenario: " + scene.scenario_title +"\n"
+        p+=s
+        c = 1
+        for given in scene.given():
+            if c == 1:
+                g = "Given "
+            else:
+                g = "And "
+            isi = given.text()
+            g = "\t\t" + g + isi + "\n"
+            c+=1
+        p+=g
+        c = 1
+        for when in scene.when():
+            if c == 1:
+                w = "when "
+            else:
+                w = "And "
+            isi = when.text()
+            w = "\t\t" + w + isi + "\n"
+            c+=1
+        p+=w
+        c = 1
+        for then in scene.then():
+            if c == 1:
+                t = "then "
+            else:
+                t = "And "
+            isi = then.text()
+            t = "\t\t" + t + isi + "\n"
+            c+=1
+        p+=t
+        p+="\n"
+    text = text.replace('<s>',p)
+
+    # write file dari data diatas
+    file = open(filepath,'w')
+    file.write(text)
+    file.close()
+
     # Open the file for reading content
     path = open(filepath, 'r')
     # Set the mime type
@@ -444,6 +502,6 @@ def download(request, id):
     # Set the return value of the HttpResponse
     response = HttpResponse(path, content_type=mime_type)
     # Set the HTTP header for sending to browser
-    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    response['Content-Disposition'] = "attachment; filename=%s" % request.session["project_name"] + "_uss.feature"
     # Return the response value
     return response
